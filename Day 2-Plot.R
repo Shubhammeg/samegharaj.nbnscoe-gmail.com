febed12044a052f393da202b5a0117dccc069534
Shubhammeg <63388791+Shubhammeg@users.noreply.github.com>
2020-04-22 19:18:28 +0530

    Add files via upload

---
+++ Day 2-Plot.R
@@ -0,0 +1,20 @@
+SAM<-datasets::airquality
+head(SAM)
+tail(SAM)
+SAM[,c(1,2,3)]
+SAM$Ozone
+SAM$Temp
+summary(SAM)
+summary(SAM$Day)
+plot(SAM$Ozone)
+plot(SAM$Ozone,SAM$Solar.R)
+plot(SAM)
+plot(SAM$Wind,type = "b")
+plot(SAM$Ozone, type = "p",xlab = 'ozone type',ylab = "instance", main = "ozone lvel",col = "blue")
+barplot(SAM$Ozone, xlab = "anna", ylab = "amma", col = "yellow", main = "don")
+hist(SAM$Solar.R)
+hist(SAM$Solar.R, main = "amma", xlab = "aaa", ylab = "yyy", col = "green")
+boxplot(SAM$Ozone, SAM$Month)
+boxplot(SAM[,c(2:4)])
+
+
